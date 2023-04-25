edc9ded5a4676d3d497b855329cffda88cc8b3bc
Floodnut <gsniper777@gmail.com>
2023-04-25 15:25:19 +0900

    feat: 테라폼 백엔드 경로 변경 (#1)

--- packer/script/03_update_terraform_state.sh
+++ packer/script/03_update_terraform_state.sh
@@ -2,6 +2,6 @@
 set -ex
 S3_BUCKET=`aws s3 ls --region $AWS_REGION | grep terraform-state | tail -n1 | cut -d ' ' -f3`
 
-sed -i 's/terraform-state-init-cloud/'${S3_BUCKET}'/' backend.tf
-sed -i 's/#//g' backend.tf
+sed -i 's/terraform-state-init-cloud/'${S3_BUCKET}'/' ./terraform/dev/backend.tf
+sed -i 's/#//g' ./terraform/dev/backend.tf
 aws s3 cp s3://${S3_BUCKET}/ami.tf ami.tf --region $AWS_REGION
