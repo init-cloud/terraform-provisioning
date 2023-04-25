2c23ab8bbf6067bbe9202c12895489c7c4855c78
Floodnut <gsniper777@gmail.com>
2023-04-25 15:14:34 +0900

    feat: 테라폼 및 패커 파이프라인 테스트 (#1)

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -1,16 +1,3 @@
-/*
-  @floodnut 2023.04.22 최초 작성
-  @floodnut 2023.04.22 수정
-*/
-
-resource "aws_s3_bucket" "terraform-state" {
-  bucket = "terraform-state-init-cloud"
-
-  tags = {
-    Name = "Terraform state"
-  }
-}
-
 terraform {
   backend "s3" {
     bucket = "terraform-state-init-cloud"
